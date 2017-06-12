601b1aeab43bd1bbf3771687ff185b5873931b50
Akin909 <akin.sowemimo@gmail.com>
2017-06-12 17:37:17 +0100

    Add neighbour calculations

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import styles from './Cell.css';
+//import React from 'react';
+//import styles from './Cell.css';
 
 function Cell(props) {
-  const height = 1;
-  const width = 1;
+  const height = 20;
+  const width = 20;
   const { ctx, x, y } = props;
   ctx.fillStyle = '#FF0000';
   ctx.fillRect(x, y, width, height);
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import styles from './Game.css';
 import Cell from './Cell.jsx';
 
 class Game extends Component {
@@ -11,39 +10,86 @@ class Game extends Component {
     this.updateCanvas();
   }
 
-  createArray(rows) {
-    const arr = [];
-    for (let i = 0; i < rows; i++) {
-      arr[i] = [];
-    }
-    return arr;
+  createLifeAtRandom(gridWidth, cellArea) {
+    const possibleRows = gridWidth / cellArea;
+    const theGrid = Array.from({ length: possibleRows }, (row, i) => {
+      row = Array.from({ length: possibleRows }, column => {
+        column = Math.floor(Math.random() * 2);
+        return column;
+      });
+      return row;
+    });
+    return theGrid;
   }
 
-  createLifeAtRandom(theGrid, gridWidth, gridHeight) {
-    for (let i = 0; i < gridHeight; i++) {
-      for (let j = 0; j < gridWidth; j++) {
-        const random = Math.floor(Math.random() * 2);
-        random === 1 ? (theGrid[i][j] = 1) : (theGrid[i][j] = 0);
+  findNeighbours(theGrid, x, y) {
+    if (x + 1 > 29 || x - 1 < 0 || y + 1 > 29 || y - 1 < 0) return 0;
+    const left = theGrid[x][y - 1];
+    const right = theGrid[x][y + 1];
+    const top = theGrid[x + 1][y];
+    const bottom = theGrid[x][y + 1];
+    const topLeft = theGrid[x + 1][y - 1];
+    const topRight = theGrid[x + 1][y + 1];
+    const bottomLeft = theGrid[x - 1][y - 1];
+    const bottomRight = theGrid[x - 1][y + 1];
+
+    const totalNeighbour = [
+      left,
+      right,
+      top,
+      bottom,
+      topLeft,
+      topRight,
+      bottomLeft,
+      bottomRight
+    ].reduce((acc, direction) => {
+      if (direction) {
+        acc += direction;
+        return acc;
       }
+      return acc;
+    }, 0);
+    return totalNeighbour;
+  }
+
+  aliveOrDead(neighbours) {
+    switch (true) {
+      case neighbours > 3:
+        return false;
+      case neighbours >= 2:
+        return true;
+      case neighbours === 3:
+        return true;
+      case neighbours === 2:
+        return true;
+      case neighbours < 2:
+        return false;
+      default:
+        return true;
     }
-    return theGrid;
   }
 
   updateCanvas() {
     const grdH = 600;
     const grdW = 600;
-    const theGrid = this.createArray(600);
-    const initGrid = this.createLifeAtRandom(theGrid, grdH, grdW);
     const ctx = this.refs.canvas.getContext('2d');
     ctx.clearRect(0, 0, 600, 600);
     //Draw Children
-    for (let i = 1; i < grdH; i++) {
-      for (let j = 1; j < grdW; j++) {
-        if (initGrid[i][j] === 1) {
-          Cell({ ctx, x: i, y: j });
-        }
+    //console.log('createArray', this.createLifeAtRandom(grdW, 20));
+    const initGrid = this.createLifeAtRandom(grdW, 20);
+    initGrid.forEach((rowElement, i) => {
+      if (Array.isArray(rowElement)) {
+        rowElement.forEach((item, j) => {
+          if (item === 1) {
+            let neighbours = this.findNeighbours(initGrid, i, j);
+            let alive = this.aliveOrDead(neighbours);
+            if (alive) {
+              Cell({ ctx, x: 20 * i, y: 20 * j });
+            }
+          }
+        });
       }
-    }
+    });
   }
 
   render() {
